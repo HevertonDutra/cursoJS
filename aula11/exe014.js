154bd6abef1d33d9ea4ae69fd195bf52cf1e368b
Heverton <lhevertonld@gmail.com>
2020-02-17 23:14:11 -0300

    Aula11 exercícios If/Else

---
+++ aula11/exe014.js
@@ -0,0 +1,9 @@
+var carro = 'UNO';
+console.log(`O seu carro é um ${carro} novo?`);
+if (carro != "Uno") {
+    console.log(`SIM!!! Então ele não chega a velocidade da luz pois o Uno de firma <br>
+    é o único que chega e o seu carro é um <strong>${carro}</strong> e não um super <strong>UNO de Firma.</strong>`);
+}else{
+    console.log(`SIM!!! Há, então vai Chega a velocidade da luz pq é um <strong>${carro}</strong> de firma, 
+    <br> com escada no teto e adesivo da SKY!!`);
+}
