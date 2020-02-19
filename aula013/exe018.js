cba4505c52c243488bcceb0a3682113ae8fd34b7
Heverton Dutra <heverton.dutra@triersistemas.corp>
2020-02-19 12:58:04 -0300

    Aula 13 For while Do

---
+++ aula013/exe018.js
@@ -0,0 +1,21 @@
+//Teste no final do codigo
+/*var c =1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) {
+    
+}
+//Teste no início do codigo
+var c = 1
+while (c <= 7) {
+    console.log(`Passo ${c}`)
+    c++    
+}
+*/
+//debugs
+console.log('Vai começar...')
+for(var c = 1;c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
--- aulaExercicio/projeto01/exe016/script.js
+++ aulaExercicio/projeto01/exe016/script.js
@@ -1,8 +1,8 @@
 function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
+    var hora = new Date().getHours()
+    //var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA
