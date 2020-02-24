ee83f7616a5ca5344d9fb9773193cba6c901d96e
Heverton <lhevertonld@gmail.com>
2020-02-24 17:21:25 -0300

    Tabuada milgrau

--- aulaExercicio/projeto01/exe019/script.js
+++ aulaExercicio/projeto01/exe019/script.js
@@ -1,1 +1,12 @@
-alert("Ola!")
+function tabuda() {
+  let numero = document.getElementById('txtn');
+  let res = Number(txtn.value);
+    
+} if (num.value.length == 0) {
+    res.innerHTML = 'Tenta outra'
+    window.alert = '[ERRO] bora amigix' 
+
+} else {
+    let num = Number(txtn.value);
+
+}
